128c79d78764994a422397b74b1055596a5127b7
Divyanshu Bhoyar <divyanshubhoyar@yahoo.com>
2022-09-02 09:17:13 +0530

    feat: statics ready

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,11 +4,16 @@ import { loggerLink } from "@trpc/client/links/loggerLink";
 import { withTRPC } from "@trpc/next";
 import type { AppType } from "next/dist/shared/lib/utils";
 import superjson from "superjson";
+import Layout from "../components/layout";
 import type { AppRouter } from "../server/router";
 import "../styles/globals.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
 };
 
 const getBaseUrl = () => {
